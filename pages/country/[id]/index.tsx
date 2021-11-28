121b839d7800889b7deaf739105c860aa2c52d23
shubhamdeodia <shubh.deodia@gmail.com>
2021-11-28 19:32:00 +0530

    fix(ssr) enable ssr

--- pages/country/[id]/index.tsx
+++ pages/country/[id]/index.tsx
@@ -1,11 +1,75 @@
-import { InferGetStaticPropsType, GetStaticProps } from "next";
+import {
+  InferGetStaticPropsType,
+  GetStaticProps,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from "next";
 import { useRouter } from "next/dist/client/router";
 import { Country, BorderingCountry } from "@/interface/Countries";
 import { CountryDetails } from "@/components/molecules/CountryDetails/CountryDetails";
 import { Centered } from "@/components/atoms/Layout.styled";
 import { Meta } from "@/components/molecules/Meta/Meta";
 
-export const getStaticProps: GetStaticProps<
+// export const getStaticProps: GetStaticProps<
+//   {
+//     country: Country[];
+//     borderingCountry: BorderingCountry[] | [];
+//   },
+//   { id: string }
+// > = async (context) => {
+//   const id = context.params?.id;
+
+//   const res = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
+
+//   const country: Country[] = await res.json();
+
+//   let borderingCountry: BorderingCountry[] | [] = [];
+
+//   if (country && country[0].borders && country[0].borders.length > 0) {
+//     const borderCountryCode = country[0].borders.join(",");
+
+//     if (borderCountryCode) {
+//       const borderCountryRes = await fetch(
+//         `https://restcountries.com/v3.1/alpha?codes=${borderCountryCode}`
+//       );
+//       const countryBorderData: Country[] = await borderCountryRes.json();
+
+//       borderingCountry =
+//         countryBorderData?.map((country) => ({
+//           name: country.name,
+//           flags: country.flags,
+//           population: country.population,
+//           cca3: country.cca3,
+//         })) || [];
+//     }
+//   }
+
+//   return {
+//     props: {
+//       country,
+//       borderingCountry,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch("https://restcountries.com/v3.1/all");
+
+//   const countries: Country[] = await res.json();
+
+//   const paths = countries.map((country) => ({
+//     params: {
+//       id: country.cca3,
+//     },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+
+export const getServerSideProps: GetServerSideProps<
   {
     country: Country[];
     borderingCountry: BorderingCountry[] | [];
@@ -13,22 +77,16 @@ export const getStaticProps: GetStaticProps<
   { id: string }
 > = async (context) => {
   const id = context.params?.id;
-
   const res = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
-
   const country: Country[] = await res.json();
-
   let borderingCountry: BorderingCountry[] | [] = [];
-
   if (country && country[0].borders && country[0].borders.length > 0) {
     const borderCountryCode = country[0].borders.join(",");
-
     if (borderCountryCode) {
       const borderCountryRes = await fetch(
         `https://restcountries.com/v3.1/alpha?codes=${borderCountryCode}`
       );
       const countryBorderData: Country[] = await borderCountryRes.json();
-
       borderingCountry =
         countryBorderData?.map((country) => ({
           name: country.name,
@@ -38,7 +96,6 @@ export const getStaticProps: GetStaticProps<
         })) || [];
     }
   }
-
   return {
     props: {
       country,
@@ -47,27 +104,10 @@ export const getStaticProps: GetStaticProps<
   };
 };
 
-export const getStaticPaths = async () => {
-  const res = await fetch("https://restcountries.com/v3.1/all");
-
-  const countries: Country[] = await res.json();
-
-  const paths = countries.map((country) => ({
-    params: {
-      id: country.cca3,
-    },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
 const CountriesDetails = ({
   country,
   borderingCountry,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
 
   const { id } = router.query;
